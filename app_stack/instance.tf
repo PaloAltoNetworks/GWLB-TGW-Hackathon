a25130b226d82e351fafa5ebb0827f4726d4fcfd
thomasbeaumont <thom.beaumont@gmail.com>
2021-04-09 14:41:07 +0200

    Update instance.tf

--- app_stack/instance.tf
+++ app_stack/instance.tf
@@ -14,17 +14,14 @@ resource "aws_network_interface" "app-eni" {
   }
 }
 
-# ---------------------------------------------------------------------------------------------------------------------
-# Commented to save an Elastic IP (CloudShare Quota)
-# ---------------------------------------------------------------------------------------------------------------------
-#resource "aws_eip" "app-mgmt-eip" {
-#  vpc               = true
-#  network_interface = aws_network_interface.app-eni.id
-#  tags = {
-#    Name = "app-mgmt-eip-${random_id.deployment_id.hex}"
-#  }
-#  depends_on = [aws_instance.app_instance]
-#}
+resource "aws_eip" "app-mgmt-eip" {
+  vpc               = true
+  network_interface = aws_network_interface.app-eni.id
+  tags = {
+    Name = "app-mgmt-eip-${random_id.deployment_id.hex}"
+  }
+  depends_on = [aws_instance.app_instance]
+}
 
 # ---------------------------------------------------------------------------------------------------------------------
 # CREATE WEB SERVER INSTANCE
