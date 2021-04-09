19f3488ad1d146b1d5f657c2081bf78982840004
thomasbeaumont <thom.beaumont@gmail.com>
2021-04-09 14:45:39 +0200

    Update output.tf

--- app_stack/output.tf
+++ app_stack/output.tf
@@ -7,9 +7,9 @@ output "app_fqdn" {
   value = aws_alb.alb.dns_name
 }
 
-output "app_mgmt_ip" {
-  value = aws_eip.app-mgmt-eip.public_ip
-}
+#output "app_mgmt_ip" {
+#  value = aws_eip.app-mgmt-eip.public_ip
+#}
 
 output "app_gwlbe_id" {
   value = jsondecode(data.local_file.gwlbe.content).agwe_id
